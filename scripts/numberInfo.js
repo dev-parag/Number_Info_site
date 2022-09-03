1dfc16cc46cb4525ed2cf57ec2ab3e64b2ab569e
Kushal K S <kushalkishor31@gmail.com>
2022-09-03 11:49:08 +0530

    Added boldness to output`s results, formated some code.
    Added boldness to the important parts of the results, formatted the JS code to handle big numbers.

--- scripts/numberInfo.js
+++ scripts/numberInfo.js
@@ -1,36 +1,31 @@
 function numberInfo(inputValue) {
-    let primeResult;
-
-
-    // Checking if the number is too big/small to calculate.
-    if (inputValue > 1000000 || inputValue < -1000000) {
-        primeResult = "Number is too big to check. :("
-    }
-
-    // Collecting all results to one result.
-    else {
-        primeResult = primeChecker(inputValue)
-    }
-
     // Setting the output.
-    document.getElementById("out1").innerText = evenOddChecker(inputValue)
-    document.getElementById("out2").innerText = primeResult
-    document.getElementById("out3").innerText = perfectSquareChecker(inputValue)
+    document.getElementById("out1").innerHTML = evenOddChecker(inputValue)
+    document.getElementById("out2").innerHTML = primeChecker(inputValue)
+    document.getElementById("out3").innerHTML = perfectSquareChecker(inputValue)
 }
 
 function evenOddChecker(num) {
     let evenOddAns;
 
-    if (num % 2 == 0) {
-        evenOddAns = num + " is an even number."
+    // Checking if the number is too big/small to calculate.
+    if (num > 9000000000000000) {
+        evenOddAns = "Number is too big to check. :("
     }
 
+    // Checking if num is even or odd.
     else {
-        evenOddAns = num + " is an odd number."
-    }
+        if (num % 2 == 0) {
+            evenOddAns = num + " is an <b>even</b> number."
+        }
 
-    if (num == 0) {
-        evenOddAns = "0 is an even number."
+        else {
+            evenOddAns = num + " is an <b>odd</b> number."
+        }
+
+        if (num == 0) {
+            evenOddAns = "0 is an <b>even</b> number."
+        }
     }
 
     return evenOddAns;
@@ -40,32 +35,40 @@ function primeChecker(num) {
     let primeAns;
     flag = 0;
 
-    if (num < 0) {
-        for (let i = -2; i > num; i--) {
-            if (num % i == 0) {
-                flag = 1;
+    // Checking if the number is too big/small to calculate.
+    if (num > 1000000 || num < -1000000) {
+        primeAns = "Number is too big to check. :("
+    }
+
+    // Checking if num is prime or not.
+    else {
+        if (num < 0) {
+            for (let i = -2; i > num; i--) {
+                if (num % i == 0) {
+                    flag = 1;
+                }
             }
         }
-    }
 
-    else {
-        for (let i = 2; i < num; i++) {
-            if (num % i == 0) {
-                flag = 1;
+        else {
+            for (let i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    flag = 1;
+                }
             }
         }
-    }
 
-    if (flag == 0) {
-        primeAns = num + " is a prime number.";
-    }
+        if (flag == 0) {
+            primeAns = num + " is a <b>prime</b> number.";
+        }
 
-    else {
-        primeAns = num + " is not a prime number.";
-    }
+        else {
+            primeAns = num + " is <b>not a prime</b> number.";
+        }
 
-    if (num == 0) {
-        primeAns = "0 is neither prime nor a composite number."
+        if (num == 0) {
+            primeAns = "0 is <b>neither prime nor a composite</b> number."
+        }
     }
 
     return primeAns;
@@ -82,22 +85,22 @@ function perfectSquareChecker(num) {
         fixer = ""
     }
 
-    else{
+    else {
         inverter = -1;
         iota = "i (iota)";
         fixer = "not"
     }
 
-    if (Math.pow(num * inverter,0.5) % 1 == 0 ) {
-        squareAns = num + " is " + fixer + " a perfect square, its square root is " + (Math.pow(num * inverter,0.5)) + iota;
+    if (Math.pow(num * inverter, 0.5) % 1 == 0) {
+        squareAns = num + " is " + fixer + " a <b>perfect square</b>, its square root is <b>" + (Math.pow(num * inverter, 0.5)) + iota + "</b>";
     }
 
-    else{
-        squareAns = num + " is not a perfect square, its approximate square root is " + (Math.pow(num * inverter,0.5)).toFixed(3) + iota;
+    else {
+        squareAns = num + " is <b>not a perfect square</b>, its approximate square root is <b>" + (Math.pow(num * inverter, 0.5)).toFixed(3) + iota + "</b>";
     }
 
     if (num == 0) {
-        squareAns = "0 is a perfect square, its square root is 0."
+        squareAns = "0 is a <b>perfect square</b>, its square root is <b>0</b>"
     }
 
     return squareAns;
